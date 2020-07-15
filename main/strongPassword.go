e1e762efb590b4ebe3141723e84d089b3d44360b
paspartuuu <danworkmail@yandex.ru>
2020-07-15 15:19:00 +0300

    delete comments

--- main/strongPassword.go
+++ main/strongPassword.go
@@ -11,27 +11,18 @@ import (
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	//Инициализация и ввод пароля
 	var pw string
 	fmt.Print("Enter password: ")
 	_, _ = fmt.Scan(&pw)
 
-	//Инициализация примера безопасного пароля и присваивание ему значения pw в ASCII коде
 	pwEx := []byte(pw)
 
-	//Инициализация цикла для проверки безопасности пароля
 	for true {
-		//Если пароль является безопасным (содержит больше 5 символов, хотя бы 1 специальный символ,
-		//строчную и заглавную буквы английского алфавита, и цифру),
-		//то выводится соответствующее сообщение и происходит выход из цикла
 		if len(pw) > 5 && checkSpecChar(pw) && checkUpCase(pw) && checkLowCase(pw) && checkDigit(pw) {
 			fmt.Println("Your password is strong")
 			break
 		} else {
-			//Если пароль не является безопасным, выводится соответствующее сообщение
 			fmt.Println("Your password isn't strong.\nYou should add: ")
-			//Если пароль меньше необходимой длины, на экран выводится
-			//количесво недостающих символов
 			if len(pw) < 6 {
 				fmt.Print(" - at least ", 6-len(pw))
 				if 6-len(pw) > 1 {
@@ -40,8 +31,6 @@ func main() {
 					fmt.Println(" symbol")
 				}
 			}
-			//Далее  информация обо всех недостающих символах, если они имеются, выводится на экран
-			//и в pwEx добавляется соответствующий случайный символ
 			if !checkSpecChar(pw) {
 				fmt.Printf(" - at least 1 special character \"%s\"\n", random.Symbols)
 				pwEx = append(pwEx, random.Symbols[rand.Intn(len(random.Symbols))])
@@ -59,29 +48,23 @@ func main() {
 				pwEx = append(pwEx, byte(48+rand.Intn(10)))
 			}
 
-			//Если pwEx меньше необходимой длины, к нему добавляются недостающие случайные символы
-			//из списка строчных букв английского алфавита
 			if len(pwEx) < 6 {
 				for i, g := 0, len(pwEx); i < 6-g; i++ {
 					pwEx = append(pwEx, byte(97+rand.Intn(26)))
 				}
 			}
 
-			//Вывод на экран примера безопасного пароля
 			fmt.Println("Example:", string(pwEx))
 
-			//Ввод нового пароля
 			fmt.Print("\nEnter password: ")
 			_, _ = fmt.Scan(&pw)
 			fmt.Println()
 
-			//Присваивание примеру пароля значения pw в ASCII коде
 			pwEx = []byte(pw)
 		}
 	}
 }
 
-//Функция проверяет строку s на наличие хотя бы 1 специального символа
 func checkSpecChar(s string) (b bool) {
 	for i := 0; i < len(s); i++ {
 		if strings.Contains(random.Symbols, string(s[i])) {
@@ -93,7 +76,6 @@ func checkSpecChar(s string) (b bool) {
 	return b
 }
 
-//Функция проверяет строку s на наличие хотя бы 1 заглавной буквы из английского алфавита
 func checkUpCase(s string) (b bool) {
 	for i := 0; i < len(s); i++ {
 		if s[i] > 64 && s[i] < 91 {
@@ -105,7 +87,6 @@ func checkUpCase(s string) (b bool) {
 	return b
 }
 
-//Функция проверяет строку s на наличие хотя бы 1 строчной буквы из английского алфавита
 func checkLowCase(s string) (b bool) {
 	for i := 0; i < len(s); i++ {
 		if s[i] > 96 && s[i] < 123 {
@@ -117,7 +98,6 @@ func checkLowCase(s string) (b bool) {
 	return b
 }
 
-//Функция проверяет строку s на наличие хотя бы 1 цифры.
 func checkDigit(s string) (b bool) {
 	for i := 0; i < len(s); i++ {
 		if strings.Contains("0123456789", string(s[i])) {

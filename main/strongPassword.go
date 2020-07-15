cb48f211220c8d1a7c2281b4fede5ef7cbfbb611
paspartuuu <danworkmail@yandex.ru>
2020-07-15 10:48:04 +0300

    add base functions

--- main/strongPassword.go
+++ main/strongPassword.go
@@ -14,15 +14,40 @@ func main() {
 	fmt.Print("Enter password: ")
 	fmt.Scan(&pw)
 	fmt.Println()
-	fxPw := make([]byte, 6)
+
+	fxPw := []byte(pw)
 
 	for true {
 
 		if len(pw) > 5 && checkSpecChar(pw) {
 			fmt.Println("Your password is strong")
-			//break
+			break
 		} else {
-			fmt.Println("Your password isn't strong.\nYou should add at least 1")
+			fmt.Println("Your password isn't strong.\nYou should add: ")
+			if len(pw) < 6 {
+				fmt.Print(" - at least ", 6-len(pw))
+				if 6-len(pw) > 1 {
+					fmt.Println(" symbols")
+				} else {
+					fmt.Println(" symbol")
+				}
+
+			}
+			if !checkSpecChar(pw) {
+				fmt.Println(" - at least 1 special character \"!@#$%^&*()-+_\"")
+
+			}
+			if !checkUpCase(pw) {
+				fmt.Println(" - at least 1 uppercase English character")
+			}
+			if !checkLowCase(pw) {
+				fmt.Println(" - at least 1 lowercase English character")
+			}
+			if !checkDigit(pw) {
+				fmt.Println(" - at least 1 digit")
+			}
+
+			break
 		}
 	}
 }
@@ -30,6 +55,39 @@ func main() {
 func checkSpecChar(s string) (ch bool) {
 	for i := 0; i < len(s); i++ {
 		if strings.Contains("!@#$%^&*()-+_", string(s[i])) {
+			ch = true
+			return ch
+		}
+	}
+	ch = false
+	return ch
+}
+
+func checkUpCase(s string) (ch bool) {
+	for i := 0; i < len(s); i++ {
+		if s[i] > 64 && s[i] < 91 {
+			ch = true
+			return ch
+		}
+	}
+	ch = false
+	return ch
+}
+
+func checkLowCase(s string) (ch bool) {
+	for i := 0; i < len(s); i++ {
+		if s[i] > 96 && s[i] < 123 {
+			ch = true
+			return ch
+		}
+	}
+	ch = false
+	return ch
+}
+
+func checkDigit(s string) (ch bool) {
+	for i := 0; i < len(s); i++ {
+		if strings.Contains("0123456789", string(s[i])) {
 			ch = true
 			return ch
 		}

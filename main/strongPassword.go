d23692f2d75fcb34f40e21ae4348d649d2139f9b
paspartuuu <danworkmail@yandex.ru>
2020-07-15 11:57:45 +0300

    working program

--- main/strongPassword.go
+++ main/strongPassword.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/labstack/gommon/random"
 	"math/rand"
 	"strings"
 	"time"
@@ -13,13 +14,11 @@ func main() {
 	var pw string
 	fmt.Print("Enter password: ")
 	fmt.Scan(&pw)
-	fmt.Println()
 
-	fxPw := []byte(pw)
+	pwEx := []byte(pw)
 
 	for true {
-
-		if len(pw) > 5 && checkSpecChar(pw) {
+		if len(pw) > 5 && checkSpecChar(pw) && checkUpCase(pw) && checkLowCase(pw) && checkDigit(pw) {
 			fmt.Println("Your password is strong")
 			break
 		} else {
@@ -31,30 +30,43 @@ func main() {
 				} else {
 					fmt.Println(" symbol")
 				}
-
 			}
 			if !checkSpecChar(pw) {
-				fmt.Println(" - at least 1 special character \"!@#$%^&*()-+_\"")
-
+				fmt.Printf(" - at least 1 special character \"%s\"\n", random.Symbols)
+				pwEx = append(pwEx, random.Symbols[rand.Intn(len(random.Symbols))])
 			}
 			if !checkUpCase(pw) {
 				fmt.Println(" - at least 1 uppercase English character")
+				pwEx = append(pwEx, byte(65+rand.Intn(26)))
 			}
 			if !checkLowCase(pw) {
 				fmt.Println(" - at least 1 lowercase English character")
+				pwEx = append(pwEx, byte(97+rand.Intn(26)))
 			}
 			if !checkDigit(pw) {
 				fmt.Println(" - at least 1 digit")
+				pwEx = append(pwEx, byte(48+rand.Intn(10)))
+			}
+			if len(pwEx) < 6 {
+				for i, g := 0, len(pwEx); i < 6-g; i++ {
+					pwEx = append(pwEx, byte(97+rand.Intn(26)))
+				}
 			}
 
-			break
+			fmt.Println("Example:", string(pwEx))
+
+			fmt.Print("\nEnter password: ")
+			fmt.Scan(&pw)
+			fmt.Println()
+
+			pwEx = []byte(pw)
 		}
 	}
 }
 
 func checkSpecChar(s string) (ch bool) {
 	for i := 0; i < len(s); i++ {
-		if strings.Contains("!@#$%^&*()-+_", string(s[i])) {
+		if strings.Contains(random.Symbols, string(s[i])) {
 			ch = true
 			return ch
 		}
